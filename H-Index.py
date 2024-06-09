b3f1fbcdbfeb4fab6398b543b8752dbc5fe83a78
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-09 11:12:31 +0330

    Solved H-Index (274)

---
+++ H-Index.py
@@ -0,0 +1,18 @@
+def hIndex(citations: list[int]) -> int:
+    h_index = 0
+    h_map = {}
+    for i in range(len(citations)):
+        if citations[i] not in h_map:
+            h_map[citations[i]] = 0
+            for j in range(len(citations)):
+                if citations[j] >= citations[i]:
+                    h_map[citations[i]] += 1
+
+    for key, value in h_map.items():
+        if key > h_index:
+            if value >= key:
+                h_index = key
+            elif min(key, value) > h_index:
+                h_index = min(key, value)
+
+    return h_index
