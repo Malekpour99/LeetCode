a907d92be065cfe6f1ff64e1ca2af59351222bb9
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-09 14:24:54 +0330

    Added optimized solution for Add Two Numbers

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -4,7 +4,8 @@ class ListNode:
         self.val = val
         self.next = next
 
-def addTwoNumbers(l1:ListNode, l2:ListNode):
+
+def addTwoNumbers(l1: ListNode, l2: ListNode):
     num1 = ""
     num2 = ""
     while l1 is not None:
@@ -13,12 +14,39 @@ def addTwoNumbers(l1:ListNode, l2:ListNode):
     while l2 is not None:
         num2 = str(l2.val) + num2
         l2 = l2.next
-    
+
     sum = int(num1) + int(num2)
     ans = [None] * len(str(sum))
     for i, val in enumerate(reversed(str(sum))):
         ans[i] = ListNode(int(val))
-    for i in range(len(ans)-1):
-        ans[i].next = ans[i+1]
-        
+    for i in range(len(ans) - 1):
+        ans[i].next = ans[i + 1]
+
     return ans[0]
+
+
+# Optimized solution from Leet Code
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
+
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
