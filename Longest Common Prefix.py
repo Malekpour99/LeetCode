b93bc93e248d790dab1907172720c212ff47fd95
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-11 10:09:33 +0330

    Added Longest Common Prefix - Optimized Solution

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -12,3 +12,17 @@ def longestCommonPrefix(strs: list[str]) -> str:
             return lcp
         lcp += temp_lcp
         i += 1
+
+
+# Optimized solution from Leet Code
+class Solution:
+    def longestCommonPrefix(self, v: list[str]) -> str:
+        ans = ""
+        v = sorted(v)
+        first = v[0]
+        last = v[-1]
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return ans
+            ans += first[i]
+        return ans
