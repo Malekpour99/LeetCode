e560cda50ab889776b446332c78144821bc39d1b
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-14 19:00:02 +0330

    Binary Search (704) solution - update
    Updated code login - nothing was really wrong

--- Binary Search.py
+++ Binary Search.py
@@ -1,14 +1,14 @@
 def search(nums: list[int], target: int) -> int:
     low = 0
-    high = len(nums)
-    while low < high:
+    high = len(nums) - 1 
+    while low <= high:
         mid = (high + low) // 2
         if target == nums[mid]:
             return mid
         elif target > nums[mid]:
             low = mid + 1
         else:
-            high = mid
+            high = mid - 1 
     
     # If target was not found
     return -1
