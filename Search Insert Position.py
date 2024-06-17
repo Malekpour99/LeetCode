c52d3ab039ce05974e009e8cd2028554202dc458
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-17 20:58:30 +0330

    Solved Search Insert Position (35)
    Optimal solution

---
+++ Search Insert Position.py
@@ -0,0 +1,15 @@
+def searchInsert(nums: list[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if target == nums[mid]:
+            return mid
+        elif target > nums[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    if target > nums[mid]:
+        return mid + 1
+    return mid
