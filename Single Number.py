f436fc372bb172770939cbd4c879879b46c13ac1
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-29 05:10:07 +0330

    Single Number (136) optimal solution
    Used XOR for reaching the optimal solution based on both time and space complexity

--- Single Number.py
+++ Single Number.py
@@ -7,3 +7,19 @@ def singleNumber(nums: list[int]) -> int:
             num_map[nums[i]] += 1
 
     return min(num_map, key=num_map.get)
+
+# Optimal Solution
+def singleNumber(nums: list[int]) -> int:
+    xor = 0
+    for num in nums:
+        xor ^= num
+
+    return xor
+
+
+# Xor of any two num gives the difference of bit as 1 and same bit as 0.
+# Thus, using this we get 1 ^ 1 == 0 because the same numbers have same bits.
+# So, we will always get the single element because all the same ones will evaluate to 0
+# and 0^single_number = single_number.
+# Time: O(n)
+# Space: O(1)
